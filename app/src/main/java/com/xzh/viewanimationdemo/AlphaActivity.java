73f741f1a90182eb9e2c6410e7f57912305722d2
1322739583 <1322739583@qq.com>
2022-03-16 00:33:15 +0800

    完成四个最基本的动画

--- app/src/main/java/com/xzh/viewanimationdemo/AlphaActivity.java
+++ app/src/main/java/com/xzh/viewanimationdemo/AlphaActivity.java
@@ -4,15 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 public class AlphaActivity extends AppCompatActivity {
+
+    private View alphaView;
+    private AlphaAnimation alphaAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alpha);
+        alphaView = findViewById(R.id.view);
+
+        alphaAnimation = new AlphaAnimation(0,1);
+        alphaAnimation.setFillAfter(true);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+        alphaAnimation.setRepeatCount(Animation.INFINITE);
+
+
+
     }
 
     public void start(View view) {
+        alphaView.startAnimation(alphaAnimation);
     }
 }
--- app/src/main/java/com/xzh/viewanimationdemo/ScaleActivity.java
+++ app/src/main/java/com/xzh/viewanimationdemo/ScaleActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 
@@ -17,8 +18,13 @@ public class ScaleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scale);
         scaleView = findViewById(R.id.view);
-        scaleAnimation = new ScaleAnimation(0,1.5f,0,1.5f,100,100);
+        //scaleAnimation = new ScaleAnimation(0,1.5f,0,1.5f,100,100);
+        scaleAnimation = new ScaleAnimation(1.0f,2.0f,1.0f,2.0f,Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,0 );
+
         scaleAnimation.setDuration(2000);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setRepeatMode(Animation.REVERSE);
+        scaleAnimation.setRepeatCount(Animation.INFINITE);
     }
 
     public void start(View view) {
